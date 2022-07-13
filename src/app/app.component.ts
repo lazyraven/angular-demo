1018d24659d7f7e0a9502d70e58e36352dc9223e
Yadav <703328798@genpact.com>
2022-07-13 12:18:13 +0530

    API integration get and post and list display and ngModel, http request implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UserDataService } from './services/user-data.service'
+import { NgForm } from '@angular/forms';
+// import { FormControl, FormGroup, Validators } from '@angular/forms'
+
 
 @Component({
   selector: 'app-root',
@@ -11,4 +15,25 @@ export class AppComponent {
   toggleForm() {
     this.formDisplay = !this.formDisplay;
   }
+
+  userList:any;
+  constructor(private userDetail: UserDataService) {
+    console.log("userInfo", userDetail, userDetail.userListData())
+    this.userDetail.userListData().subscribe((data: any) => {
+      console.log("data", data)
+      this.userList = data
+    })
+  }
+
+  getUserFormData(data: any) {
+    console.log(data)
+    this.userDetail.saveUser(data).subscribe((result: any) => {
+      console.log("data post",data, result)
+    })
+  }
+  
+  // userFormData = new FormGroup({
+  //   title: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+  //   body: new FormControl('', [Validators.required, Validators.minLength(100)])
+  // })
 }
---
+++ src/app/services/user-data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+  url = 'https://jsonplaceholder.typicode.com/posts'
+
+  constructor(private http: HttpClient) { }
+  userListData() {
+    return this.http.get(this.url)
+  }
+  saveUser(data:any){
+    return this.http.post(this.url, data)
+  }
+}
